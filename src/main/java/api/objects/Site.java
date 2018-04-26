524a4297013da751965bc957ec9b0ee31c000356
maxim <laba1203@gmail.com>
2018-04-26 17:38:35 +0300

    API scenarios have been added

---
+++ src/main/java/api/objects/Site.java
@@ -0,0 +1,21 @@
+package api.objects;
+
+public class Site {
+    private String apiKey;
+    private String siteSlug;
+
+    public Site setData(String siteSlug, String apiKey){
+        this.apiKey = apiKey;
+        this.siteSlug = siteSlug;
+
+        return this;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public String getSiteSlug() {
+        return siteSlug;
+    }
+}
---
+++ src/main/java/api/objects/origin/Origin.java
@@ -0,0 +1,55 @@
+package api.objects.origin;
+
+import api.objects.Site;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import util.EnvFactory;
+
+
+public class Origin{
+
+    private static final String URL = EnvFactory.getApiUrl() + "/origins";
+
+    public Response postOriginPurchaseWithUUID(Site site, String email, String uuid){
+        Response response = RestAssured
+                .given()
+                .contentType("application/json")
+                .body("{\n" +
+                        "  \"api_key\": \"" + site.getApiKey() + "\",\n" +
+                        "  \"site_slug\": \"" + site.getSiteSlug() + "\",\n" +
+                        "  \"type\": \"Purchase\",\n" +
+                        "  \"data\": {\n" +
+                        "    \"email\": \"" + email + "\",\n" +
+                        "    \"order_number\": " + String.valueOf(System.currentTimeMillis()).substring(7) + ",\n" +
+                        "    \"subtotal\": 100,\n" +
+                        "    \"uuid\": \"" + uuid + "\",\n" +
+                        "    \"campaign_tags\": \"api\",\n" +
+                        "    \"items\": [\n" +
+                        "      {\n" +
+                        "        \"price\": 25,\n" +
+                        "        \"quantity\": 4,\n" +
+                        "        \"product_id\": \"TSHIRT\"\n" +
+                        "      }\n" +
+                        "    ]\n" +
+                        "  }\n" +
+                        "}")
+                .when()
+                .post(URL);
+        System.out.println("LOG: Response body for POST to /origin :");
+        response.body().print();
+        return response;
+    }
+
+
+    public Response postOriginPurchase(Site site){
+        String defaultUUID = "b3967d36-4e7f-46bc-92b3-57344347cd6a";
+        String defaultEmail = "api.email@test.com";
+        return postOriginPurchaseWithUUID(site, defaultEmail, defaultUUID);
+    }
+
+
+
+
+
+
+}
