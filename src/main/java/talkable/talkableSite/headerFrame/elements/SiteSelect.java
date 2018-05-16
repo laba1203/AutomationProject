e09fba4e7202f8cdacb215308cb7b2281c485aee
maxim <laba1203@gmail.com>
2018-05-16 17:33:17 +0300

    stale element issue

--- src/main/java/talkable/talkableSite/headerFrame/elements/SiteSelect.java
+++ src/main/java/talkable/talkableSite/headerFrame/elements/SiteSelect.java
@@ -2,17 +2,27 @@ package talkable.talkableSite.headerFrame.elements;
 
 import abstractObjects.AbstractSelectElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 
 public class SiteSelect extends AbstractSelectElement{
 
-//    private static final By locator = By.className("js-sites-select");
     private static final String xpathToSelect = "//select[contains(@class,'js-sites-select')]";
-    private WebElement selectWebElement = getDriver().findElement(By.xpath("//select[contains(@class,'js-sites-select')]"));
+//    private WebElement selectWebElement = getDriver().findElement(By.xpath("//select[contains(@class,'js-sites-select')]"));
+    private WebElement selectWebElement = getDriver().findElement(By.xpath(xpathToSelect));
 
     public SiteSelect(){
-//        setWebElement(xpathToSelect);
-        setWebElement(selectWebElement);
+        try {
+            initializeWebElement();
+            setWebElement(selectWebElement);
+        }catch (StaleElementReferenceException e){
+             initializeWebElement();
+             setWebElement(selectWebElement);
+        }
+    }
+
+    private void initializeWebElement(){
+        selectWebElement = getDriver().findElement(By.xpath(xpathToSelect));
     }
 
 
