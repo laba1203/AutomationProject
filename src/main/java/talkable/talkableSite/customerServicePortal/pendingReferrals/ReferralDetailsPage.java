7ff87e001acb4ba5fb595892faccf229775038b0
maxim <laba1203@gmail.com>
2018-07-11 10:23:14 +0300

    fixed createReferral method

--- src/main/java/talkable/talkableSite/customerServicePortal/pendingReferrals/ReferralDetailsPage.java
+++ src/main/java/talkable/talkableSite/customerServicePortal/pendingReferrals/ReferralDetailsPage.java
@@ -8,7 +8,9 @@ public class ReferralDetailsPage extends AbstractCustomerServicePortalPage{
 
     private static final By referralStatusLctr = By.xpath("//*[contains(@class, 'status-text')]");
 
+    private Element referralStatus = new Element(referralStatusLctr);
+
     public String getReferralStatus(){
-        return new Element(referralStatusLctr).getText().substring(17);
+        return referralStatus.getText().substring(17);
     }
 }
