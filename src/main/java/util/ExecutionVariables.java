f6f20020da90923f8a4a25ffe880ed1fd5891cd2
maxim <laba1203@gmail.com>
2018-07-12 18:01:42 +0300

    quit() removed

--- src/main/java/util/ExecutionVariables.java
+++ src/main/java/util/ExecutionVariables.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 public class ExecutionVariables {
     /*The flag indicates whether the test will be executed on Local or Remote machine.
     !!! Please note always put the flag to TRUE before creation of pull request to origin/master !!!*/
-    private static final boolean remoteExecution = false;
+    private static final boolean remoteExecution = true;
 
     public static boolean remoteExecution(){
         return remoteExecution;
@@ -14,11 +14,11 @@ public class ExecutionVariables {
     public static String getMavenEnvName(){
         //property described in pom.xml  <test.environment>${env.NAME}</test.environment>
         //correct working:
-//        String env = System.getProperty("test.environment");
+        String env = System.getProperty("test.environment");
         //for local running of the project:
 //        String env = "VOID";
 //        String env = "PROD";
-        String env = "BASTION";
+//        String env = "BASTION";
 
         System.out.println("LOG - EnvFactory: Test is running on <" + env + "> environment");
         if(env == null){
