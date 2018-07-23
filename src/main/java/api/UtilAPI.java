4af5a3666db506492b095583bff950c22ba579db
maxim <laba1203@gmail.com>
2018-07-23 16:07:27 +0300

    logs

--- src/main/java/api/UtilAPI.java
+++ src/main/java/api/UtilAPI.java
@@ -3,8 +3,11 @@ package api;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
+import util.logging.Log;
 
 import java.util.Arrays;
+
+import static io.restassured.http.ContentType.JSON;
 
 public class UtilAPI {
 
@@ -16,10 +19,21 @@ public class UtilAPI {
     public static void logResponse(Response resp){
         System.out.println("LOG: API Response:");
         System.out.println(resp.getStatusLine());
-        System.out.println("Headers:");
-        System.out.print(Arrays.toString(resp.getHeaders().asList().toArray()));
+        System.out.print("Headers:");
+        System.out.println(Arrays.toString(resp.getHeaders().asList().toArray()));
         System.out.println("Body:");
         resp.body().prettyPrint();
-        System.out.println("  *******  ");
+        System.out.println("  * * * * End response  * * *  ");
     }
+
+    public static boolean isResultOk(Response response){
+        boolean result = response.then()
+                .contentType(JSON)
+                .extract()
+                .path("ok");
+        Log.logRecord("Is response successful({\"ok\"}): " + result);
+        return result;
+    }
+
+
 }
