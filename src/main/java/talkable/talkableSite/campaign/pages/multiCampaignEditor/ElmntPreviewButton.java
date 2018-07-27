cf1ae2a7d5fb4601cc1e1f0f35f3e555986d30dc
maxim <laba1203@gmail.com>
2018-07-27 12:49:58 +0300

    updated locator

--- src/main/java/talkable/talkableSite/campaign/pages/multiCampaignEditor/ElmntPreviewButton.java
+++ src/main/java/talkable/talkableSite/campaign/pages/multiCampaignEditor/ElmntPreviewButton.java
@@ -4,7 +4,7 @@ import abstractObjects.AbstractElement;
 import org.openqa.selenium.By;
 
 class ElmntPreviewButton extends AbstractElement{
-    private static final By locator = By.cssSelector(".editMandatoryFields-field-grid > .base-btn");
+    private static final By locator = By.cssSelector(".edit-field-grid > .base-btn");
 
     ElmntPreviewButton(){
         setWebElement(locator);
